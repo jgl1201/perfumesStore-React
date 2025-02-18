4c4ab4bd23540ec2dede1386bf66c7089605db50
Jorge García López <a24jorgegl@teis.intranet>
2025-02-18 13:31:13 +0100

    feat!: Routes to Login and Register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Store from './pages/Store';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Store />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );

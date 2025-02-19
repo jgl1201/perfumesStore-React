a21975c1283500186213e140d07aa394eaa262ce
jgl1201 <jorgegarciacebem@gmail.com>
2025-02-19 11:54:59 +0100

    feat: NavBar implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Store from './pages/Store';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,6 +8,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Store />} />
         <Route path="/login" element={<Login />} />

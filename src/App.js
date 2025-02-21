5d95546030b55b7f9715bf12f54412a7ad2e2e09
Jorge García López <a24jorgegl@teis.intranet>
2025-02-21 09:05:44 +0100

    fix: cart route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import Store from './pages/Store';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Store />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );

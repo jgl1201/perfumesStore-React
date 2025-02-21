5ffe20fe4f6926d63722d71977e8cbe24be185a3
Jorge García López <a24jorgegl@teis.intranet>
2025-02-21 09:15:33 +0100

    feat: Cart into navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,6 +52,9 @@ const NavBar = () => {
                 {/*Link to the Store*/}
                 <Link to="/" className="navbar-brand mx-auto">Store</Link>
 
+                {/*Link to Cart*/}
+                <Link to="/cart" className="nav-link"><i className="bi bi-bag-fill"></i> Cart</Link>
+
                 {/*Button to toggle the NavBar at small screens*/}
                 <button className="navbar-toggler" type="button" dara-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>

3d33ded2e6ef135e7654f1146f1610ce4cd4e6e4
Jorge García López <a24jorgegl@teis.intranet>
2025-02-21 10:08:07 +0100

    style: NavBar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,47 +41,60 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
+                {/* Link to the Store */}
+                <Link to="/" className="navbar-brand">
+                    <i className="bi bi-shop-window"></i> Store
+                </Link>
 
-                {/*User (if logged) */}
-                {isLogged && (
-                    <span className="navbar-text me-3">
-                        <i className="bi bi-person-circle"></i> {username}
-                    </span>
-                )}
+                {/* Link to Cart */}
+                <Link to="/cart" className="nav-link me-3">
+                    <i className="bi bi-cart-fill"></i> Cart
+                </Link>
 
-                {/*Link to the Store*/}
-                <Link to="/" className="nav-link mx-auto mx-lg-3"><i className="bi bi-shop-window"></i> Store</Link>
-
-                {/*Link to Cart*/}
-                <Link to="/cart" className="nav-link ms-lg-3"><i className="bi bi-cart-fill"></i> Cart</Link>
-
-                {/*Button to toggle the NavBar at small screens*/}
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                {/* Button to toggle the NavBar at small screens */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                {/*NavBar toggler items*/}
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        {/*Show Login and Register only if the user is not Logged*/}
+                {/* NavBar toggler items */}
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        {/* User (if logged) */}
+                        {isLogged && (
+                            <li className="nav-item">
+                                <span className="nav-link">
+                                    <i className="bi bi-person-circle"></i> {username}
+                                </span>
+                            </li>
+                        )}
+
+                        {/* Show Login and Register only if the user is not Logged */}
                         {!isLogged && (
                             <>
-                            {/*Login*/}
-                            <li className="nav-item">
-                                <Link to="/login" className="nav-link">
-                                    <i className="bi bi-box-arrow-in-right"></i> Login
-                                </Link>
-                            </li>
-                            {/*Register*/}
-                            <li className="nav-item">
-                                <Link to="/register" className="nav-link">
-                                    <i className="bi bi-person-plus"></i> Register
-                                </Link>
-                            </li>
+                                {/* Login */}
+                                <li className="nav-item">
+                                    <Link to="/login" className="nav-link">
+                                        <i className="bi bi-box-arrow-in-right"></i> Login
+                                    </Link>
+                                </li>
+                                {/* Register */}
+                                <li className="nav-item">
+                                    <Link to="/register" className="nav-link">
+                                        <i className="bi bi-person-plus"></i> Register
+                                    </Link>
+                                </li>
                             </>
                         )}
-                        
-                        {/*Show Logout only if the user is Logged*/}
+
+                        {/* Show Logout only if the user is Logged */}
                         {isLogged && (
                             <li className="nav-item">
                                 <button className="nav-link btn btn-link" onClick={handleLogout}>

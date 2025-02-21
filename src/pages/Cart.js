b94e9b07e3e771764bee4226ddaf5b39dbc9d527
Jorge García López <a24jorgegl@teis.intranet>
2025-02-21 08:29:07 +0100

    feat: fetch stored cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Cart= () => {
 
-    //TODO: cart logic
+    const [carItems, setCartItems] = useState([]); //* State to save the items at the cart
+    const navigate = useNavigate();
+
+    //* Load cart items from localStorage
+    useEffect(()  => {
+        const storedCart = JSON.parse(localStorage.getItem('cart')) || [];
+        setCartItems(storedCart);
+    }, []); // useEffect
 
 };
 

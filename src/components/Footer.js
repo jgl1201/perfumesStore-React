6a29a921f43acbe93836354ebd7b4405f5a93e28
jgl1201 <jorgegarciacebem@gmail.com>
2025-03-03 14:22:22 +0100

    feat: Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                <strong>Legal:</strong> This page has been created for educational purposes only. The products shown are just examples of existing prodicts and are not intended to tarnish the image or harm any brand mentioned. All rights reserved to their respective owners.
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
